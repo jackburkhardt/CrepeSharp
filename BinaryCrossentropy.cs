24089e1517c3d1dcd700fdc6ca968962bd5a28ed
Jack Burkhardt <43016465+jackburkhardt@users.noreply.github.com>
2022-08-08 22:03:55 -0500

    model building done

---
+++ BinaryCrossentropy.cs
@@ -0,0 +1,26 @@
+﻿using Tensorflow;
+using Tensorflow.Keras.Losses;
+using static Tensorflow.Binding;
+
+namespace CrepeSharp
+{
+    public class BinaryCrossentropy : LossFunctionWrapper, ILossFunc
+    {
+        public BinaryCrossentropy(string reduction = "auto", string name = null, bool from_logits = false) 
+            : base(reduction, name, from_logits)
+        {
+        }
+
+        // todo: does this even work right??
+        public override Tensor Apply(Tensor x, Tensor y, bool from_logits = false, int axis = -1)
+        {
+            var shape = tf.reduce_prod(tf.shape(x));
+            var count = tf.cast(shape, TF_DataType.TF_FLOAT);
+            x = tf.clip_by_value(x, 1e-6f, 1.0f - 1e-6f);
+            var z = y * tf.log(x) + (1 - y) * tf.log(1 - x);
+            var result = -1.0f / count * tf.reduce_sum(z);
+            return result;
+        }
+        
+    }
+}
--- CrepeSharp.cs
+++ CrepeSharp.cs
@@ -1,39 +1,105 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Tensorflow;
-using Tensorflow.Keras.ArgsDefinition;
 using Tensorflow.Keras.Engine;
-using Tensorflow.Keras.Layers;
+using Tensorflow.Keras.Optimizers;
+using static Tensorflow.KerasApi;
+using static Tensorflow.Binding;
 
 namespace CrepeSharp
 {
     public static class CrepeSharp
     {
         private const int MODEL_SRATE = 16000;
+        private static Dictionary<ModelCapacity, Model> models = new Dictionary<ModelCapacity, Model>()
+        {
+            {ModelCapacity.Tiny, null},
+            {ModelCapacity.Small, null},
+            {ModelCapacity.Medium, null},
+            {ModelCapacity.Large, null},
+            {ModelCapacity.Full, null}
+        };
 
+        /// <summary>
+        /// Build the CNN model and load the weights
+        /// </summary>
+        /// <param name="capacity">String specifying the model capacity, which determines the model's
+        ///                         capacity multiplier to 4 (tiny), 8 (small), 16 (medium), 24 (large),
+        ///                         or 32 (full). 'full' uses the model size specified in the paper,
+        ///                         and the others use a reduced number of filters in each convolutional
+        ///                         layer, resulting in a smaller model that is faster to evaluate at the
+        ///                         cost of slightly reduced pitch estimation accuracy.</param>
+        /// <returns>The pre-trained keras model loaded in memory</returns>
         public static Model BuildAndLoadModel(ModelCapacity capacity)
         {
+            if (models[capacity] != null) return models[capacity];
+            
             var capacity_multiplier = 4 * ((int)capacity + 1);
             var layers = new[] {1, 2, 3, 4, 5, 6};
-
+            var filters = new[] { 32, 4, 4, 4, 8, 16 };
+            for (int i = 0; i < filters.Length; i++)
+            {
+                filters[i] *= capacity_multiplier;
+            }
             var widths = new[] {512, 64, 64, 64, 64, 64};
             var strides = new[] {(4, 1), (1, 1), (1, 1), (1, 1), (1, 1), (1, 1)};
+            
+            var x = keras.Input(shape: (1024), name: "input", dtype: TF_DataType.TF_FLOAT);
+            var y = tf.reshape(tensor: x, shape: (1024, 1, 1), name: "input-reshape");
 
-            var inputLayerArgs = new InputLayerArgs
+            // todo: make sure layers length doesn't change
+            for (int i = 0; i < layers.Length; i++)
             {
-                InputShape = (1024),
-                Name = "input",
-                DType = TF_DataType.TF_FLOAT
-            };
+                var l = layers[i];
+                var f = filters[i];
+                var w = widths[i];
+                var s = strides[i];
 
-            var reshapeArgs = new ReshapeArgs
+                keras.layers.Conv2D(f, (w, 1), strides: s, padding: "same", activation: "relu").Apply(y);
+                keras.layers.BatchNormalization().Apply(y);
+                keras.layers.MaxPooling2D(pool_size: (2, 1), strides: null, padding: "valid").Apply(y);
+                keras.layers.Dropout(rate: 0.25f).Apply(y);
+            }
+
+            keras.layers.Permute(new []{2,1,3}).Apply(y);
+            keras.layers.Flatten().Apply(y);
+            keras.layers.Dense(360, activation: "sigmoid");
+
+            var model = keras.Model(x, y);
+
+            var package_dir = Directory.GetCurrentDirectory();
+            var filename = $"model-{capacity.ToString()}.h5";
+            
+            try
             {
-                TargetShape = (1024, 1, 1),
-                Name = "input-reshape"
-            };
+                model.load_weights($"{package_dir}{filename}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
 
-            var x = new InputLayer(inputLayerArgs);
-            var y = new Reshape(reshapeArgs);
- 
+            model.compile(optimizer: new Adam(), loss: new BinaryCrossentropy());
+            //model.compile(new Adam()); // TODO: binary crossentropy needed
+
+            models[capacity] = model;
+
+            return model;
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="suffix"></param>
+        /// <param name="output_dir"></param>
+        /// <returns>The output path of an output file corresponding to a wav file</returns>
+        public static string OutputPath(object file, string suffix, string output_dir)
+        {
+        }
+        
+        public static 
     }
 }
